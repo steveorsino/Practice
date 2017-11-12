37a647b0ffb1431e6eda9430e524989eec65ada6
Steve Orsino <steveorsino@yahoo.com>
2017-11-12 14:32:20 +0000

    fixed bug where non usercould see signed in state

--- main.cpp
+++ main.cpp
@@ -138,6 +138,96 @@ int main()
       if(user2.getLastName() != "Doe")
       {
         cout << "Welcome back " << user2.getFirstName() << " " << user2.getLastName() << endl;
+        
+        fstream journalFile;
+        journalFile.open("journals.txt", ios::in);
+        journalcount = 0;
+        string jInput;
+        
+        if(!journalFile)
+        {
+          cout << "\nERROR\n";
+        }
+        
+        while(journalFile >> jInput)
+        {
+          if(jInput == user2.getUserName())
+          {
+            journalcount++;
+          }
+        }
+        journalFile.close();
+        
+        cout << "\nYou have " << journalcount << " journal entries.\n";
+        
+        char choice2;
+        do {
+  
+          cout << "What would you like to do today, " << user2.getFirstName() << "?\n";
+          cout << "\tA. create a new journal entry\n"
+                << "\tB. Display your entries\n"
+                << "\tC. Exit\n"
+                << "Enter your choice: ";
+          cin >> choice2;
+          
+          if (choice2 == 'A' || choice2 == 'a')
+          {
+            cin.ignore();
+            cout << "Enter a ttile for your new Journal entry: ";
+            getline(cin, jTitle);
+            cout << "Enter the body of your new Journal entry: ";
+            getline(cin, jBody);
+            
+            cout << "Title: " << jTitle << endl;
+            cout << "Body: " << jBody << endl;
+            
+            journal1.setUserName(user2.getUserName());
+            journal1.setTitle(jTitle);
+            journal1.setBody(jBody);
+            
+            journalFile.open("journals.txt", ios::app);
+            journalFile << journal1.getUser() << endl;
+            journalFile << journal1.getTitle() << endl;
+            journalFile << journal1.getBody() << endl;
+            journalFile << journal1.getTime() << endl;
+            journalFile.close();
+          }
+          
+          else if (choice2 == 'B' || choice2 == 'b')
+          {
+             cin.ignore();
+             journalFile.open("journals.txt", ios::in);
+             while(getline(journalFile,jInput))
+              {
+                if(jInput == user2.getUserName())
+                {
+                  cout << "User: " << jInput << endl;
+                  getline(journalFile,jInput);
+                  cout << "Title: " << jInput << endl;
+                  getline(journalFile,jInput);
+                  cout <<"Body: " << jInput << endl;
+                  journalFile >> jInput;
+                  cout << "Timestamp: " << jInput << endl << endl; 
+                  //cin.ignore();
+                }
+              }
+              journalFile.close();
+          }
+          
+          else if (choice2 == 'C' || choice2 == 'c')
+          {
+            cout << "Back to main menu.\n";
+          }
+          
+          else
+          {
+            cout << "Invalid choice.\n\n";
+          }
+          
+        } while (choice2 != 'C' && choice2 != 'c');  
+        
+        
+        
       }
       else
       {
@@ -146,92 +236,7 @@ int main()
       }
       dataFile.close();
       
-      fstream journalFile;
-      journalFile.open("journals.txt", ios::in);
-      journalcount = 0;
-      string jInput;
       
-      if(!journalFile)
-      {
-        cout << "\nERROR\n";
-      }
-      
-      while(journalFile >> jInput)
-      {
-        if(jInput == user2.getUserName())
-        {
-          journalcount++;
-        }
-      }
-      journalFile.close();
-      
-      cout << "\nYou have " << journalcount << " journal entries.\n";
-      
-      char choice2;
-      do {
-
-        cout << "What would you like to do today, " << user2.getFirstName() << "?\n";
-        cout << "\tA. create a new journal entry\n"
-              << "\tB. Display your entries\n"
-              << "\tC. Exit\n"
-              << "Enter your choice: ";
-        cin >> choice2;
-        
-        if (choice2 == 'A' || choice2 == 'a')
-        {
-          cin.ignore();
-          cout << "Enter a ttile for your new Journal entry: ";
-          getline(cin, jTitle);
-          cout << "Enter the body of your new Journal entry: ";
-          getline(cin, jBody);
-          
-          cout << "Title: " << jTitle << endl;
-          cout << "Body: " << jBody << endl;
-          
-          journal1.setUserName(user2.getUserName());
-          journal1.setTitle(jTitle);
-          journal1.setBody(jBody);
-          
-          journalFile.open("journals.txt", ios::app);
-          journalFile << journal1.getUser() << endl;
-          journalFile << journal1.getTitle() << endl;
-          journalFile << journal1.getBody() << endl;
-          journalFile << journal1.getTime() << endl;
-          journalFile.close();
-        }
-        
-        else if (choice2 == 'B' || choice2 == 'b')
-        {
-           cin.ignore();
-           journalFile.open("journals.txt", ios::in);
-           while(getline(journalFile,jInput))
-            {
-              if(jInput == user2.getUserName())
-              {
-                cout << "User: " << jInput << endl;
-                getline(journalFile,jInput);
-                cout << "Title: " << jInput << endl;
-                getline(journalFile,jInput);
-                cout <<"Body: " << jInput << endl;
-                journalFile >> jInput;
-                cout << "Timestamp: " << jInput << endl << endl; 
-                //cin.ignore();
-              }
-            }
-            journalFile.close();
-        }
-        
-        else if (choice2 == 'C' || choice2 == 'c')
-        {
-          cout << "Back to main menu.\n";
-        }
-        
-        else
-        {
-          cout << "Invalid choice.\n\n";
-        }
-        
-      } while (choice2 != 'C' && choice2 != 'c');
       
       
       
